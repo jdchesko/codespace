eb6296006a30a6472fb99ceacfd51e0ab5bf7a26
jdchesko <114958459+jdchesko@users.noreply.github.com>
2023-04-06 16:18:22 +0000

    Thu, Apr 6, 2023, 4:18 PM +00:00

--- population/population.c
+++ population/population.c
@@ -4,10 +4,19 @@
 int main(void)
 {
     // TODO: Prompt for start size
-     int n
+     int start
+     do while (start <9)
+     {
+        start = get_int("input starting size?");
+     }
     // TODO: Prompt for end size
-
+     int endsize
+     do while (endsize <10)
+     {
+        endsize = get_int("input ending size:");
+     }
     // TODO: Calculate number of years until we reach threshold
-
+       int populationNow
+       
     // TODO: Print number of years
 }
