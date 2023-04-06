17f117ba33d407a5b3754d181955678cbd3350b2
jdchesko <114958459+jdchesko@users.noreply.github.com>
2023-04-06 20:54:58 +0000

    Thu, Apr 6, 2023, 8:54 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+ do
+ {
+    int rows = get_int("input number of rows between 1 and 8:");
+ }
+ while (rows <1) || (rows>8)
+ for (int counter=0; counter < rows; counter++ )
+   
 }
